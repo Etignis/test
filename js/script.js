aa553671ab324a605812d43d5058644ca7151d97
Etignis <etignis@gmail.com>
2020-04-17 13:28:53 +0300

    fixes

--- js/script.js
+++ js/script.js
@@ -2,15 +2,23 @@ document.addEventListener('DOMContentLoaded', function(){
 	let bPlay = false;
 	let oPlayButton = document.querySelector("#play");
 	let oPauseButton = document.querySelector("#pause");
+	let oPrevButton = document.querySelector("#prev");
+	let oNextButton = document.querySelector("#next");
 	let oOpenButton = document.querySelector("#open");
 	let oOpenFile = document.querySelector("#files");
 	let oAudio = document.querySelector("#audio");
 	let oTime = document.querySelector("#time");
 	let oTitle = document.querySelector("#title");
-	let oScale = document.querySelector("#progress_input");
+	let oProgress = document.querySelector("#progress_input");
+	let oVolume = document.querySelector("#volume_input");
 	let aMusic = [];
 	let nTrackIndex = 0;
 	let nDuration = 0;
+	
+	let oColors = {};
+	
+	let aRanges = document.querySelectorAll("input[type='range']");
+	
 	oOpenButton.onclick = function(){
 		oOpenFile.click();
 	}
@@ -18,14 +26,12 @@ document.addEventListener('DOMContentLoaded', function(){
 		var fileList = this.files; /* теперь вы можете работь со списком файлов */
 		aMusic = this.files;
 		oAudio.src = URL.createObjectURL(aMusic[nTrackIndex]);
+		play();
 	}
+		
 	
 	oAudio.onend = function(e) {
-		nTrackIndex++;
-		if(nTrackIndex<aMusic.length){
-			oAudio.src = URL.createObjectURL(aMusic[nTrackIndex]);
-			play();
-		}
+		next();
   }
 	oAudio.addEventListener('timeupdate', function(){
 		showDurationProgress();
@@ -36,8 +42,16 @@ document.addEventListener('DOMContentLoaded', function(){
 		showDurationProgress();
 	}
 	
+
+	
 	oPlayButton.onclick = play;
 	oPauseButton.onclick = pause;
+	oPrevButton.onclick = prev;
+	oNextButton.onclick = next;
+	
+	oProgress.addEventListener('change',  time_change); 
+	oVolume.addEventListener('change',  volume_change); 
+
 	
 	function setTitle(){
 		let sTitle = aMusic[nTrackIndex].name;
@@ -45,14 +59,14 @@ document.addEventListener('DOMContentLoaded', function(){
 	}
 	function getTime(n){
 		//let nMinutes =  n%60;
-		let nSeconds = ~~(n<60? n :n/60);
+		let nSeconds = ~~(n<60? n :n%60);
 		if(nSeconds<10)
 			nSeconds="0"+nSeconds;
-		let nMinutes = n<60? 0 : ~~(n%60);
+		let nMinutes = n<60? 0 : ~~(n/60);
 		return `${nMinutes}:${nSeconds}`;
 	}
 	function getPercent(nCur, nMax){
-		return ~~(nCur*nMax/100);
+		return ~~(nCur*100/nMax);
 	}
 	function showDurationProgress (){
 		let nFull = getTime(nDuration)
@@ -60,24 +74,101 @@ document.addEventListener('DOMContentLoaded', function(){
 		oTime.innerText = `${nCurrent}/${nFull}`;
 		
 		let nPerc = getPercent(oAudio.currentTime, nDuration);
-		oScale.value = nPerc;
+		oProgress.value = nPerc;
 		
-		oScale.style.background = `linear-gradient(90deg, #d44 ${nPerc}%, #00000099 ${nPerc+0.1}%)`;
+		oProgress.style.background = `linear-gradient(90deg, ${oColors['main-color']} ${nPerc}%, ${oColors['color']} ${nPerc+0.1}%)`;
 	}
 	
+	function _play(){
+		if(oAudio.readyState == 4){
+						oPlayButton.style.display = 'none';
+						oPauseButton.style.display = 'flex';
+						oAudio.play();
+						setTitle();
+					} else {
+						alert("Треки не выбраны или еще не загружены");
+					}
+	}
 	function play(){
-		oPlayButton.style.display = 'none';
-		oPauseButton.style.display = 'flex';
-		oAudio.play();
-		setTitle();
-	} 
+		if(oAudio.readyState<4) {
+			oAudio.addEventListener('canplay', function() {
+					_play();
+				},
+				{
+					once: true
+				}
+			);			
+		} else {
+			_play();
+		}
+	}
+	
 	function pause(){
 		oPlayButton.style.display = 'flex';
 		oPauseButton.style.display = 'none';
 		oAudio.pause();
 	}
 
+	function next(){
+		nTrackIndex++;
+		if(nTrackIndex<aMusic.length){
+			oAudio.src = URL.createObjectURL(aMusic[nTrackIndex]);
+			play();
+		} else {
+			alert("Это последний трек")
+		}
+	}
+	
+	function prev(){
+		nTrackIndex--;
+		if(nTrackIndex>=0){
+			oAudio.src = URL.createObjectURL(aMusic[nTrackIndex]);
+			play();
+		} else {
+			alert("Это первый трек")
+		}
+	}
+	
+	function time_change(oEvent){
+		let nValue = Number(oEvent.target.value);
+		let nK = nDuration/100;
+		
+		let nNewTime = Math.floor(nValue*nK);
+		oAudio.currentTime = nNewTime;
+	}
+
+	function volume_change(oEvent){
+		let nValue = Number(oEvent.target.value)/100;
+		oAudio.volume = nValue; 
+	}
+
 	function init(){
+		// load colors from CSS
+		let aCssVars = ['main-color', 'color'];
+		aCssVars.forEach(function(sVar){
+			let sVal = getComputedStyle(document.documentElement).getPropertyValue(`--${sVar}`); 
+			if(sVal) {
+				oColors[sVar] = sVal.trim(); 
+			}
+		});
+		
+		// init ranges		
+		aRanges.forEach(function(oRange){
+			let nPerc = getPercent(oRange.value, 100);			
+			oRange.style.background = `linear-gradient(90deg, ${oColors['main-color']} ${nPerc}%, ${oColors['color']} ${nPerc+0.1}%)`;
+			oRange.addEventListener('mousemove', function(oEvent){
+				let nPerc = getPercent(oEvent.target.value, 100);			
+				oRange.style.background = `linear-gradient(90deg, ${oColors['main-color']} ${nPerc}%, ${oColors['color']} ${nPerc+0.1}%)`;
+			});
+			oRange.addEventListener('change', function(oEvent){
+				let nPerc = getPercent(oEvent.target.value, 100);			
+				oRange.style.background = `linear-gradient(90deg, ${oColors['main-color']} ${nPerc}%, ${oColors['color']} ${nPerc+0.1}%)`;
+			});
+		});
+		
+		Audio.volume = 0.5;
+		
+		// hide pause
 		oPauseButton.style.display = 'none';
 	}
 
